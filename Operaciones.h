83e02b9cef77f3ed33513580a17f70524a8c1e0c
Jordan Quiroga <jorshamany2030@gmail.com>
2023-07-09 22:13:10 -0600

    ultimos cambios como requisitos

--- Operaciones.h
+++ Operaciones.h
@@ -9,38 +9,38 @@ template <typename T>
 class ListNode
 {
 public:
-    T data;
-    ListNode<T>* next;
+    T data;               // Almacena los datos del nodo
+    ListNode<T>* next;    // Puntero al siguiente nodo
 
-    ListNode(T value) : data(value), next(nullptr) {}
+    ListNode(T value) : data(value), next(nullptr) {}  // Constructor que inicializa los datos del nodo
 };
 
 template <typename T>
 class List
 {
 private:
-    ListNode<T>* head;
+    ListNode<T>* head;    // Puntero al primer nodo de la lista
 
 public:
-    List() : head(nullptr) {}
+    List() : head(nullptr) {}  // Constructor que inicializa la lista vacía
 
-    List(T* in_data, int in_size) : head(nullptr)
+    List(T* in_data, int in_size) : head(nullptr)  // Constructor que inicializa la lista con datos de un array
     {
         for (int i = 0; i < in_size; i++)
         {
-            PushBack(in_data[i]);
+            PushBack(in_data[i]);  // Agrega cada elemento del array a la lista
         }
     }
 
     void PushBack(T value)
     {
-        ListNode<T>* newNode = new ListNode<T>(value);
+        ListNode<T>* newNode = new ListNode<T>(value);  // Crea un nuevo nodo con el valor especificado
 
-        if (head == nullptr)
+        if (head == nullptr)  // Si la lista está vacía, el nuevo nodo se convierte en el primer nodo
         {
             head = newNode;
         }
-        else
+        else  // Si la lista no está vacía, se agrega el nuevo nodo al final
         {
             ListNode<T>* current = head;
             while (current->next != nullptr)
@@ -54,7 +54,7 @@ public:
     void Print()
     {
         ListNode<T>* current = head;
-        while (current != nullptr)
+        while (current != nullptr)  // Recorre la lista y muestra los elementos
         {
             std::cout << current->data;
             if (current->next != nullptr)
@@ -72,7 +72,7 @@ public:
         ListNode<T>* current = head;
         ListNode<T>* next = nullptr;
 
-        while (current != nullptr)
+        while (current != nullptr)  // Invierte el orden de los nodos en la lista
         {
             next = current->next;
             current->next = prev;
@@ -80,7 +80,7 @@ public:
             current = next;
         }
 
-        head = prev;
+        head = prev;  // Actualiza el puntero al primer nodo para que apunte al último nodo (después de invertir)
     }
 
     void Unique()
@@ -92,7 +92,7 @@ public:
         while (current != nullptr && current->next != nullptr)
         {
             ListNode<T>* runner = current;
-            while (runner->next != nullptr)
+            while (runner->next != nullptr)  // Elimina los nodos duplicados en la lista
             {
                 if (runner->next->data == current->data)
                 {
@@ -113,7 +113,7 @@ public:
     ~List()
     {
         ListNode<T>* current = head;
-        while (current != nullptr)
+        while (current != nullptr)  // Libera la memoria de todos los nodos de la lista
         {
             ListNode<T>* temp = current;
             current = current->next;
@@ -123,3 +123,27 @@ public:
 };
 
 #endif // OPERACIONES_H
+
+//Referencias de las funciones:
+//Nota estas referencias o fuentes solo son utilizadas por mi y el equipo para darse una idea,
+//de como utilizar cada funcion y poder crear todo el codigo 
+//
+// void print()
+// https://cplusplus.com/forum/beginner/85507/
+// 
+// void Reverse();
+// https://www.geeksforgeeks.org/reverse-a-linked-list/
+// https://cplusplus.com/reference/algorithm/reverse/
+// 
+// void Unique()
+// https://cplusplus.com/reference/list/list/unique/
+//
+// List(T* in_data, int in_size)
+// Nota este si tuvimos que ver muchos videos y ver otra vez la clase que tuvimos donde lo vimos 
+// la siguiente pagina solo es una fuente de estructura de datos 
+// https://cplusplus.com/doc/tutorial/structures/
+
+// Nota final para terminar el codigo se tuvo que ver muchos videos de todoas las funciones y como aplicarlas esto lo hacemos para 
+// ampliar nuestro conocimiento a lo que estamos practicando de la tarea y lo anterior son paginas que nos sirvieron para investigar 
+// para que se note que no es plagio jamas hemos tomado ningun codigo copiado o solo pegado en eete proyecto de tarea.
+
